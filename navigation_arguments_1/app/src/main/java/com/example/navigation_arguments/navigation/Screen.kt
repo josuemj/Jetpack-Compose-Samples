c602d376358b2380fa698254897e2e8f65b6ba70
josuemj <98498772+josuemj@users.noreply.github.com>
2023-09-14 13:54:41 -0300

    Proyect removed

--- navigation_arguments_1/app/src/main/java/com/example/navigation_arguments/navigation/Screen.kt
+++
@@ -1,6 +0,0 @@
-package com.example.navigation_arguments.navigation
-
-sealed class Screen(val route: String){
-    object LoginScreen : Screen("login_screen")
-    object WelcomeScreen: Screen("welcome_screen")
-}
